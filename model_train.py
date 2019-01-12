e5ab5a38d9b1856568697c51287b26ced083d3aa
LichtungSq <247757808@qq.com>
2019-01-12 13:13:26 +0800

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -114,8 +114,8 @@ def train(self, begin_score, end_score):
     else:
 	    self.model = self.build()
 
-if os.path.exists("my_model2.h5"):
-    model = load_model("my_model2.h5")
+if os.path.exists("model.h5"):
+    model = load_model("model.h5")
 else:
     model = My_Model()
 task = ModelWrapper(model, 2**16)
@@ -124,12 +124,16 @@ count = 0
 
 while True:
     game = Game()
-
+    count += 1
     while not game.end:
         task.move(game)
     task.train(batch = batch)
 
-    model.save(filepath="my_model2_1.h5", overwrite=True)
+    if count % 10 == 0:
+        model.save(filepath="model.h5", overwrite=True)
+
+    if count % 100 == 0:
+        model.save(filepath="model_%d.h5" % count, overwrite=True)
 
     # count += 1
     # if count < 1000:
